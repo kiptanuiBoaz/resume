40c4ef0fd646b5d9ac6e020ffa88a5266b95aded
Boaz <boaserem022@gmail.com>
2022-07-14 15:20:47 +0300

    adding details

--- src/components/side/Details.jsx
+++ src/components/side/Details.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Details (){
     return(
         <div>
-            <p>Testing details</p>
+            <p><span></span>Eldoret, Kenya</p>
+            <p><span></span>+254743637613</p>
+            <p><span></span>boaserem022@gmail.com</p>
         </div>
     )
 }
