c12a095b276a06e0e4a6ee1dd41fce15e06d9c2f
Boaz <boaserem022@gmail.com>
2022-07-14 15:50:56 +0300

    add details

--- src/components/side/Details.jsx
+++ src/components/side/Details.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { ImLocation2} from 'react-icons/im';
+import {IoMdCall} from "react-icons/io"
+import {MdEmail} from "react-icons/md"
+
 
 function Details (){
     return(
         <div>
-            <p><span></span>Eldoret, Kenya</p>
-            <p><span></span>+254743637613</p>
-            <p><span></span>boaserem022@gmail.com</p>
+
+            <h3>Details</h3>
+            <p>
+                <span><ImLocation2/></span>&nbsp;Eldoret, Kenya
+            </p>
+            <p>
+                <span><IoMdCall/></span>&nbsp;+254743637613
+            </p>
+            <p>
+                <span><MdEmail/></span>&nbsp;boaserem022@gmail.com
+            </p>
         </div>
     )
 }
